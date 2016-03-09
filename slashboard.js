2d17795e16c563da286938db611ad1ac84075376
Bishaka Samuel <sbishaka@gmail.com>
2016-03-09 06:32:04 +0300

    Make initial commit of slashboard

--- slashboard.js
+++ slashboard.js
@@ -6,51 +6,32 @@ var
     slashboard = angular.module('slashboard',['zingchart-angularjs'])
     .controller('mainCtrl',['$scope','$http',function($scope,$http){
 
-            $scope.chart = {
-                "title":"",
-                "col":"col-xs-2",
-                "render":{
-                    width:"100%",
-                    height:"100",
-                    autoResize:true
-                },
-                "data":{
-                    "type": "null",
-                    "x": "0%",
-                    "y": "0%",
-                    "height": "100",
-                    "width": "100%",
-                    "border-width": "1px",
-                    "border-color": "#384653",
-                    "border-radius": 4,
-                    "background-color": "#fbfcf7",
-                    "title": {
-                        "text": "Users",
-                        "background-color": "none",
-                        "font-color": "#384653",
-                        "font-size": "12px",
-                        "text-align": "center",
-                        "height": "70px"
-                    },
-                    "subtitle": {
-                        "text": "1",
-                        "font-color": "#dd655f",
-                        "font-size": "24px",
-                        "bold": true,
-                        "text-align": "center",
-                        "height": "40px",
-                        "padding-top": "25%"
-                    }
-                }
-            };
-
             var ping = function(){
                 $http.get($scope.webhook_url).success(function(data){
                     $scope.slashboard = data["slashboard"] || data;
+                }).error(function(data,err){
+                    console.log(err);
+                    var msg = "", title="";
+                    switch(err){
+                        case -1: //Connection refused
+                            title = "Oops..."
+                            msg = "The connection was refused. This normally means that the server isn't turned on or it doesn't have that path";
+                        break;
+                        case 404:
+                            title = "Hmmm..."
+                            msg = "No page was found. please check if the webhook is spelled the right way";
+                        break;
+                    }
+                    sweetAlert(title, msg, "error");
                 });
             };
 
             $scope.fetch = function(){
+                ping();
+            };
+
+            $scope.goTo = function(url){
+                $scope.webhook_url = url;
                 ping();
             };
 
